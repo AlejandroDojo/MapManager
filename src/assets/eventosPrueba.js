19948e575d8d500cf615e7bb83d576cb29661957
Nicolas Chomyn <chomynnicolas2@gmail.com>
2024-06-26 10:10:24 -0400

    cambio

---
+++ src/assets/eventosPrueba.js
@@ -0,0 +1,158 @@
+export const eventosPrueba = [
+  {
+    _id: 1,
+    name: "San Juan",
+    type: ["festival"],
+    description: "Fiesta Patronal",
+    startDate: "2024/20/8",
+    endDate: "2024/22/8",
+    price: 0,
+    location: {
+      lat: "-27.348860009198418",
+      lng: "-55.85442781406063"
+    }
+  },
+  {
+    _id: 2,
+    name: "Camping",
+    type: ["social", "naturaleza"],
+    description: "Disfrutar de la naturaleza",
+    startDate: "2024/20/8",
+    endDate: "2024/23/8",
+    price: 0,
+    location: {
+      lat: "-27.319538552180447",
+      lng: "-55.86065053913495"
+    }
+  },
+  {
+    _id: 3,
+    name: "Concierto en el Parque",
+    type: ["musical"],
+    description: "Concierto al aire libre",
+    startDate: "2024/21/8",
+    endDate: "2024/21/8",
+    price: 200,
+    location: {
+      lat: "-27.335000",
+      lng: "-55.850000"
+    }
+  },
+  {
+    _id: 4,
+    name: "Feria Artesanal",
+    type: ["cultural"],
+    description: "Exposición de artesanías locales",
+    startDate: "2024/22/8",
+    endDate: "2024/24/8",
+    price: 0,
+    location: {
+      lat: "-27.328000",
+      lng: "-55.858000"
+    }
+  },
+  {
+    _id: 5,
+    name: "Carrera de Bicicletas",
+    type: ["deporte"],
+    description: "Competencia ciclística en la ciudad",
+    startDate: "2024/23/8",
+    endDate: "2024/23/8",
+    price: 150,
+    location: {
+      lat: "-27.340000",
+      lng: "-55.845000"
+    }
+  },
+  {
+    _id: 6,
+    name: "Exposición de Fotografía",
+    type: ["arte"],
+    description: "Galería de fotos de artistas locales",
+    startDate: "2024/24/8",
+    endDate: "2024/26/8",
+    price: 100,
+    location: {
+      lat: "-27.333000",
+      lng: "-55.849000"
+    }
+  },
+  {
+    _id: 7,
+    name: "Competencia de Ajedrez",
+    type: ["juegos"],
+    description: "Torneo de ajedrez para todas las edades",
+    startDate: "2024/20/8",
+    endDate: "2024/22/8",
+    price: 50,
+    location: {
+      lat: "-27.338000",
+      lng: "-55.852000"
+    }
+  },
+  {
+    _id: 8,
+    name: "Festival de Comida",
+    type: ["gastronómico"],
+    description: "Degustación de platos típicos",
+    startDate: "2024/21/8",
+    endDate: "2024/22/8",
+    price: 0,
+    location: {
+      lat: "-27.330000",
+      lng: "-55.855000"
+    }
+  },
+  {
+    _id: 9,
+    name: "Cine al Aire Libre",
+    type: ["entretenimiento"],
+    description: "Proyección de películas bajo las estrellas",
+    startDate: "2024/22/8",
+    endDate: "2024/22/8",
+    price: 0,
+    location: {
+      lat: "-27.327000",
+      lng: "-55.857000"
+    }
+  },
+  {
+    _id: 10,
+    name: "Maratón Solidaria",
+    type: ["deporte", "solidaridad"],
+    description: "Carrera a beneficio de una causa social",
+    startDate: "2024/24/8",
+    endDate: "2024/24/8",
+    price: 100,
+    location: {
+      lat: "-27.336000",
+      lng: "-55.853000"
+    }
+  },
+  {
+    _id: 11,
+    name: "Taller de Cerámica",
+    type: ["arte", "educación"],
+    description: "Curso intensivo de cerámica",
+    startDate: "2024/25/8",
+    endDate: "2024/25/8",
+    price: 300,
+    location: {
+      lat: "-27.332000",
+      lng: "-55.854000"
+    }
+  },
+  {
+    _id: 12,
+    name: "Yoga en el Parque",
+    type: ["salud", "bienestar"],
+    description: "Sesión de yoga al aire libre",
+    startDate: "2024/23/8",
+    endDate: "2024/23/8",
+    price: 50,
+    location: {
+      lat: "-27.329000",
+      lng: "-55.851000"
+    }
+  }
+];
