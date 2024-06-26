2e417beaf69e7030363e0933019d7fb29e3c70a5
Nicolas Chomyn <chomynnicolas2@gmail.com>
2024-06-25 21:57:38 -0400

    formulario con mapa

---
+++ src/components/MapPicker/MapPicker.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const MapPicker = ({ onLocationSelect }) => {
+  const [position, setPosition] = useState(null);
+
+  const LocationMarker = () => {
+    useMapEvents({
+      click(e) {
+        setPosition(e.latlng);
+        onLocationSelect(e.latlng);
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}></Marker>
+    );
+  };
+
+  return (
+    <MapContainer center={[-27.298, -55.858]} zoom={13} style={{ height: '500px', width: '500px' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LocationMarker />
+    </MapContainer>
+  );
+};
+
+export default MapPicker;
