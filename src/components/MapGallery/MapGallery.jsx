af02a0f154794932cdf005dec3fe8175d8ba6134
danigiber96 <gimenezbertolini@gmail.com>
2024-07-07 19:01:32 -0400

    Creacion del componente GalleryMap y css

--- src/components/MapGallery/MapGallery.jsx
+++ src/components/MapGallery/MapGallery.jsx
@@ -1,23 +1,37 @@
+import Pesca from '../../assets/pesca.jpg'
+import Asado from '../../assets/asado.jpg'
+import Artesania from '../../assets/artesania.jpg'
+import Cine from '../../assets/cine.jpg'
+import Yoga from '../../assets/yoga.jpg'
 import { Link } from "react-router-dom";
 import styles from "./MapGallery.module.css";
+
 const MapGallery = ({ eventos }) => {
-  
-  return (
+    return (
     <div className={styles.container}>
-      {eventos.map((evento, index) => {
-        return (
-          <div key={index} className={styles.event}>
+        {eventos.map((evento, index) => {
+            return (
+                <div key={index} className={styles.event}>
+
+                    <div> 
+                        <section>
+                            <img src={Pesca} alt="pesca" />
+                            <img src={Asado} alt="asado" />
+                            <img src={Artesania} alt="artesania" />
+                            <img src={Cine} alt="cine" />
+                            <img src={Yoga} alt="yoga" />
+                        </section>
+                    </div>
             
-            
-            <h2>{evento.name}</h2>
-            <p>{evento.description}</p>
-            <hr />
-            <Link className={styles.linkStyles} to={`/evento/${evento._id}`}>Ir al evento </Link>
-          </div>
+                <h2>{evento.name}</h2>
+                <p>{evento.description}</p>
+                <hr />
+                <Link className={styles.linkStyles} to={`/evento/${evento._id}`}>Ir al evento </Link>
+            </div>
         );
-      })}
+        })}
     </div>
-  );
+    );
 };
 
 export default MapGallery;
