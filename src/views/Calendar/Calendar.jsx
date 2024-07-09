fbca8fe4d4bceff034246dd8d8211011cd20fee8
¨Alejandra <ale.pedrozo.952@gmail.com>
2024-07-09 14:26:28 -0400

    cambiando el idioma

--- src/views/Calendar/Calendar.jsx
+++ src/views/Calendar/Calendar.jsx
@@ -1,22 +1,44 @@
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
+import  './Calendar.css'
+import "dayjs/locale/es"
+import { useState } from "react";
+
 
 const FCallendar=()=>{
+    dayjs.locale("es");
     const localizer= dayjsLocalizer(dayjs)
     const myEventsList=[
         {
             start: dayjs("2024-07-10T12:00:00").toDate(),
             end: dayjs("2024-07-11T12:00:00").toDate(),
             title: "Evento 1"
+        },
+        {
+            start: dayjs("2024-07-8T09:00:00").toDate(),
+            end: dayjs("2024-07-8T10:00:00").toDate(),
+            title: "Evento 2"
         }
+
     ]
 
     return(
-        <div>
+        <div className="calendarContainer">
             <Calendar 
                 localizer={localizer}
                 events={myEventsList}
+                views={["month", "week", "day"]}
+                defaultView="week"
+                messages={{
+                    next: "Siguiente",
+                    previous: "Anterior",
+                    today: "Hoy",
+                    month:"Mes",
+                    week: "Semana",
+                    day:"Dia"
+                }}
+
             />
         </div>
     )
