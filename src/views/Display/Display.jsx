57cce2f93cece8d7126a7d0ddde8bd5b5506263b
AlejandroDojo <ultiimately@gmail.com>
2024-07-10 22:38:21 -0400

    Register+Login End

---
+++ src/views/Display/Display.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+const Display = () => {
+    return (
+        <>
+            <Header logged={false} />
+            <h1>Página de presentación</h1>
+        </>
+    )
+}
+
+export default Display;
